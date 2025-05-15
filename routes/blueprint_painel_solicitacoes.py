9f821f6619543d34c87f0762a3a3a12d3bd97cea
Joao PC02 <jvmarques2006@gmail.com>
2025-05-15 17:50:28 -0300

    Só fazer o painel de solicitacoes

--- routes/blueprint_painel_solicitacoes.py
+++ routes/blueprint_painel_solicitacoes.py
@@ -7,23 +7,4 @@ blueprint_painel_solicitacoes = Blueprint('blueprint_painel_solicitacoes', __nam
 @blueprint_painel_solicitacoes.route('/', methods=['POST', 'GET'])
 @login_required
 def painel_solicitacoes():
-    filtro = request.args.get('filtro', 'todos')
-
-    status_banco = {
-        'pendente': 'PENDENTE',
-        'autorizada': 'AUTORIZADA',
-        'processo_compra': 'PROCESSO_COMPRA'
-    }
-
-    query = Solicitacoes.select().where(Solicitacoes.USUARIO_SOLICITANTE == current_user.id)
-    if filtro in status_banco:
-        query = query.where(Solicitacoes.STATUS == status_banco[filtro])
-
-    return render_template(
-        'painel_solicitacoes.html',
-        usuario_logado=current_user,
-        solicitacoes=query,
-        filtro=filtro
-    )
-
-
+        return render_template('painel_solicitacoes.html')
