71b69474586f65c20b7a562b4f882ad054631d5a
Joao PC02 <jvmarques2006@gmail.com>
2025-05-16 11:56:45 -0300

    Painel de solicitações funcionando, fazer a logica do processo de compra

--- routes/blueprint_painel_solicitacoes.py
+++ routes/blueprint_painel_solicitacoes.py
@@ -7,4 +7,16 @@ blueprint_painel_solicitacoes = Blueprint('blueprint_painel_solicitacoes', __nam
 @blueprint_painel_solicitacoes.route('/', methods=['POST', 'GET'])
 @login_required
 def painel_solicitacoes():
-        return render_template('painel_solicitacoes.html')
+        filtro = request.args.get('filtro', 'PENDENTE')
+        query = Solicitacoes.select().where(Solicitacoes.USUARIO_SOLICITANTE == current_user.USUARIO)
+        if filtro == 'PENDENTE':
+                query = query.where(Solicitacoes.STATUS == 'PENDENTE')
+        elif filtro == 'APROVADO':
+                query = query.where(Solicitacoes.STATUS == 'APROVADO')
+        elif filtro == 'REPROVADO':
+                query = query.where(Solicitacoes.STATUS == 'REPROVADO') 
+        elif filtro == 'COMPRA':
+                query = query.where(Solicitacoes.STATUS == 'COMPRA')
+        elif filtro == 'TODOS':
+                query = query.order_by(Solicitacoes.STATUS)
+        return render_template('painel_solicitacoes.html', solicitacoes=query, filtro=filtro, usuario_logado=current_user)
