a79fbfd3a3ac073224470031f5d8c92edd39c7eb
Joao PC02 <jvmarques2006@gmail.com>
2025-06-09 14:15:33 -0300

    Feature: Adicionado botão para o usuário alterar sua senha

--- routes/blueprint_principal.py
+++ routes/blueprint_principal.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, url_for, redirect, flash, session
+from flask import Blueprint, render_template, url_for, redirect, flash, session, request
 from flask_login import current_user, login_required, logout_user
 from decorators import role_required
+from database.database import Usuarios
 
 blueprint_principal = Blueprint('blueprint_principal', __name__)
 
@@ -17,3 +18,23 @@ def logout():
     session.pop('_flashes', None)
     flash('Você deslogou da sua conta com sucesso!', 'success')
     return redirect(url_for('blueprint_login.fazer_login'))
+
+@blueprint_principal.route('/troca-senha/<int:id>', methods=['POST', 'GET'])
+@login_required
+def troca_senha(id):
+    usuario = Usuarios.get_or_none(Usuarios.id == id)
+
+    if request.method == 'POST':
+        nova_senha_form = request.form['nova_senha'].strip()
+        confirma_nova_senha_form = request.form['confirma_nova_senha'].strip()
+        if nova_senha_form != confirma_nova_senha_form:
+            flash('As duas senhas devem ser iguais!', 'error')
+            return redirect(url_for('blueprint_painel_solicitacoes.painel_solicitacoes'))
+        else:
+            usuario.SENHA = nova_senha_form.strip()
+            usuario.save()
+            flash('Senha alterada com sucesso!', 'success')
+            logout_user() 
+            return redirect(url_for('blueprint_login.fazer_login'))
+
+    return render_template('blueprint_painel_solicitacoes.painel_solicitacoes')          
