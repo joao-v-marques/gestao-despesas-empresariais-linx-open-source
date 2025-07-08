d4a91d1760c893c49acf6a13d9b6e4ab1343baea
Joao PC02 <jvmarques2006@gmail.com>
2025-07-08 11:17:22 -0300

    main.py configurado para rodar servidor Waitress no WSGI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from waitress import serve
 from dotenv import load_dotenv
 from flask import Flask
 from configure import config_all
@@ -10,4 +11,6 @@ app.secret_key = os.getenv('SECRET_KEY', '')
 
 config_all(app)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    print('Inciando o Servidor Local...')
+    serve(app, host='0.0.0.0', port=8080)
