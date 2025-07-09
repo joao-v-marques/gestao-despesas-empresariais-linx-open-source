3f4e7929320f2aaf08b0b9537a33518381cfde0a
Joao PC02 <jvmarques2006@gmail.com>
2025-07-09 13:12:20 -0300

    Ajustado para usar waitress ou app.run()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from waitress import serve
+# from waitress import serve
 from dotenv import load_dotenv
 from flask import Flask
 from configure import config_all
@@ -11,6 +11,8 @@ app.secret_key = os.getenv('SECRET_KEY', '')
 
 config_all(app)
 
-if __name__ == "__main__":
-    print('Inciando o Servidor Local...')
-    serve(app, host='0.0.0.0', port=8080)
+app.run(debug=True)
+
+# if __name__ == "__main__":
+#     print('Inciando o Servidor Local...')
+#     serve(app, host='0.0.0.0', port=8080)
